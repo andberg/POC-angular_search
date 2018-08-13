d035fd2861c045b2bf3d57d50263ac8843ac6a62
andberg <andreaberglund@hotmail.com>
2018-08-13 22:40:29 +0200

    Now it works again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultListComponent
   ],
   imports: [
     BrowserModule
