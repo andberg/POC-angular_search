e52c0f304b6609f402d0a5c52677c1e97b65a182
andberg <andreaberglund@hotmail.com>
2018-08-13 22:05:34 +0200

    Added some files to the structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ResultListComponent
   ],
   imports: [
     BrowserModule
